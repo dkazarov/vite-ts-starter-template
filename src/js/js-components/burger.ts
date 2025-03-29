da0a01a2f0e65402ec4db90cb7e6954d38c67acb
unknown <d.kazarov1361@gmail.com>
2025-03-29 21:51:17 +0200

    change burger.js file to burger.ts file

---
+++ src/js/js-components/burger.ts
@@ -0,0 +1,56 @@
+// const burger = document.querySelector('.burger') as HTMLElement;
+// const burgerLine = document.querySelector('.burger__line') as HTMLElement;
+// const nav = document.querySelector('.nav') as HTMLElement;
+// const body = document.querySelector('body')as HTMLElement;
+// const navItem = document.querySelectorAll('.nav__item')as NodeListOf<HTMLElement>;
+
+// export const burgerOpen = (): void => {
+// 	burger.addEventListener('click', () => {
+// 		burgerLine.classList.toggle('line--hide');
+// 		burger.classList.toggle('burger--transform');
+// 		nav.classList.toggle('nav--hide');
+// 		body.classList.toggle('no-scroll');
+// 	});
+// };
+
+// export const burgerClose = () => {
+// 	navItem.forEach(el => {
+// 		el.addEventListener('click', () => {
+// 			burgerLine.classList.remove('line--hide');
+// 			burger.classList.remove('burger--transform');
+// 			nav.classList.remove('nav--hide');
+// 			body.classList.remove('no-scroll');
+// 		});
+// 	});
+// };
+
+const burger = document.querySelector('.burger') as HTMLElement | null;
+const burgerLine = document.querySelector('.burger__line') as HTMLElement | null;
+const nav = document.querySelector('.nav') as HTMLElement | null;
+const body = document.querySelector('body') as HTMLElement | null;
+const navItems = document.querySelectorAll('.nav__item') as NodeListOf<HTMLElement>;
+
+export const burgerOpen = (): void => {
+	if (!burger || !burgerLine || !nav || !body) return;
+
+	burger.addEventListener('click', () => {
+		burgerLine.classList.toggle('line--hide');
+		burger.classList.toggle('burger--transform');
+		nav.classList.toggle('nav--hide');
+		body.classList.toggle('no-scroll');
+	});
+};
+
+export const burgerClose = (): void => {
+	if (!burger || !burgerLine || !nav || !body) return;
+
+	navItems.forEach(el => {
+		el.addEventListener('click', () => {
+			burgerLine.classList.remove('line--hide');
+			burger.classList.remove('burger--transform');
+			nav.classList.remove('nav--hide');
+			body.classList.remove('no-scroll');
+		});
+	});
+};
+
