43b473331341a99d16a79866fb4d8e245f9d3412
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-04-03 21:03:53 +0300

    js: header: search: by adding the functionality to close the search by clicking in any place and fixing the clear button

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,22 @@
 const searchInput =
-  (document.querySelector(".search__input") as HTMLInputElement) || null;
-const searcIcon = document.querySelector(".search__icon") as HTMLImageElement;
+  (document.querySelector('.search__input') as HTMLInputElement) || null;
+const searcIcon = document.querySelector('.search__icon') as HTMLImageElement;
+const clearInputBtn = document.querySelector(
+  '.clear__input--search',
+) as HTMLButtonElement;
 
-searcIcon.addEventListener("click", () => {
-  searchInput.classList.toggle("toggle-show-search-icon");
+searcIcon.addEventListener('click', () => {
+  searchInput.classList.toggle('search__input--show');
+  clearInputBtn.classList.toggle('clear__input--show');
+});
+
+document.addEventListener('click', (event) => {
+  const target = event.target as HTMLElement;
+
+  if (!target.closest('search')) {
+    searchInput.classList.remove('search__input--show');
+    clearInputBtn.classList.remove('clear__input--show');
+
+    document.removeEventListener;
+  }
 });
