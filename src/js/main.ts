c47cb2343a141aa5260d7839c5afee7ef65bd0d1
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-04-05 19:52:29 +0300

    js: search: add key buttons functions

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,18 +2,38 @@ const searchInput = (document.querySelector('.search__input') as HTMLInputElemen
 const searcIcon = document.querySelector('.search__icon') as HTMLImageElement;
 const clearInputBtn = document.querySelector('.clear__input--search') as HTMLButtonElement;
 
-searcIcon.addEventListener('click', () => {
-  searchInput.classList.toggle('search__input--show');
-  clearInputBtn.classList.toggle('clear__input--show');
-});
+const toggleSearch = () => {
+  searcIcon.addEventListener('click', () => {
+    searchInput.classList.toggle('search__input--show');
+    clearInputBtn.classList.toggle('clear__input--show');
+  });
+};
 
-document.addEventListener('click', (event) => {
-  const target = event.target as HTMLElement;
+const clickToAnotherPlace = () => {
+  document.addEventListener('click', (event) => {
+    const target = event.target as HTMLElement;
 
-  if (!target.closest('search')) {
-    searchInput.classList.remove('search__input--show');
-    clearInputBtn.classList.remove('clear__input--show');
+    if (!target.closest('search')) {
+      searchInput?.classList.remove('search__input--show');
+      clearInputBtn?.classList.remove('clear__input--show');
 
-    document.removeEventListener;
-  }
-});
+      document.removeEventListener;
+    }
+  });
+};
+
+const pushButton = (key: string) => {
+  searchInput?.addEventListener('keydown', (e) => {
+    if (e.key === key) {
+      e.preventDefault();
+      searchInput?.classList.remove('search__input--show');
+      searchInput.value = '';
+    }
+  });
+};
+
+pushButton('Enter');
+pushButton('Escape');
+
+toggleSearch();
+clickToAnotherPlace();
