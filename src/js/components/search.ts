5870ad5cf3810bc65ab2e52935e32be212d7ac07
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-04-05 20:29:31 +0300

    js: search: transferring functions to a separate file

---
+++ src/js/components/search.ts
@@ -0,0 +1,34 @@
+const searchInput = (document.querySelector('.search__input') as HTMLInputElement) || null;
+const searcIcon = document.querySelector('.search__icon') as HTMLImageElement;
+const clearInputBtn = document.querySelector('.clear__input--search') as HTMLButtonElement;
+
+export const toggleSearch = () => {
+  searcIcon.addEventListener('click', () => {
+    searchInput?.classList.toggle('search__input--show');
+    clearInputBtn?.classList.toggle('clear__input--show');
+  });
+};
+
+// prettier-ignore
+export const clickToAnotherPlace = () => {
+  document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+
+      if (!target.closest('.search')) {
+        searchInput?.classList.remove('search__input--show');
+        clearInputBtn?.classList.remove('clear__input--show');
+      }
+    },
+    { once: true },
+  );
+};
+
+export const pushButton = (key: string) => {
+  searchInput?.addEventListener('keydown', (e) => {
+    if (e.key === key) {
+      e.preventDefault();
+      searchInput?.classList.remove('search__input--show');
+      searchInput.value = '';
+    }
+  });
+};
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,39 +1,9 @@
-const searchInput = (document.querySelector('.search__input') as HTMLInputElement) || null;
-const searcIcon = document.querySelector('.search__icon') as HTMLImageElement;
-const clearInputBtn = document.querySelector('.clear__input--search') as HTMLButtonElement;
+import { toggleSearch, clickToAnotherPlace, pushButton } from './components/search';
 
-const toggleSearch = () => {
-  searcIcon.addEventListener('click', () => {
-    searchInput.classList.toggle('search__input--show');
-    clearInputBtn.classList.toggle('clear__input--show');
-  });
-};
-
-const clickToAnotherPlace = () => {
-  document.addEventListener('click', (event) => {
-    const target = event.target as HTMLElement;
-
-    if (!target.closest('search')) {
-      searchInput?.classList.remove('search__input--show');
-      clearInputBtn?.classList.remove('clear__input--show');
-
-      document.removeEventListener;
-    }
-  });
-};
-
-const pushButton = (key: string) => {
-  searchInput?.addEventListener('keydown', (e) => {
-    if (e.key === key) {
-      e.preventDefault();
-      searchInput?.classList.remove('search__input--show');
-      searchInput.value = '';
-    }
-  });
-};
-
-pushButton('Enter');
-pushButton('Escape');
-
-toggleSearch();
-clickToAnotherPlace();
+document.addEventListener('DOMContentLoaded', () => {
+  //Search
+  toggleSearch();
+  clickToAnotherPlace();
+  pushButton('Enter');
+  pushButton('Escape');
+});
