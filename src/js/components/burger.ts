8853a58a346f65026c5390900c32cbbc7ce0133b
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-04-13 22:59:01 +0300

    header: burger menu: add burger menu finish

---
+++ src/js/components/burger.ts
@@ -0,0 +1,23 @@
+const burgerBtn = document.querySelector('.burger') as HTMLButtonElement;
+const burgerCenterLine = document.querySelector('.burger__center-line') as HTMLSpanElement;
+const neaderNav = document.querySelector('.header__nav ') as HTMLUListElement;
+
+export const burger = () => {
+  burgerBtn?.addEventListener('click', () => {
+    burgerBtn?.classList.toggle('burger-transform--top');
+    burgerCenterLine?.classList.toggle('--hide');
+
+    neaderNav?.classList.toggle('header__nav--show');
+  });
+};
+
+export const clickOnOutside = () => {
+  neaderNav.addEventListener('click', (event: Event) => {
+    if ((event.target as HTMLElement).closest('.header__nav')) {
+      neaderNav?.classList.remove('header__nav--show');
+
+      burgerBtn?.classList.remove('burger-transform--top');
+      burgerCenterLine?.classList.remove('--hide');
+    }
+  });
+};
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,32 +1,9 @@
 // @ts-check
-
 import { toggleSearch, clickToAnotherPlace, pushButton } from './components/search';
+import { burger, clickOnOutside } from './components/burger';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const burgerBtn = document.querySelector('.burger') as HTMLButtonElement;
-  const burgerCenterLine = document.querySelector('.burger__center-line') as HTMLSpanElement;
-  const neaderNav = document.querySelector('.header__nav ') as HTMLUListElement;
-
-  const burger = () => {
-    burgerBtn?.addEventListener('click', () => {
-      burgerBtn?.classList.toggle('burger-transform--top');
-      burgerCenterLine?.classList.toggle('--hide');
-
-      neaderNav?.classList.toggle('header__nav--show');
-    });
-  };
-
-  const clickOnOutside = () => {
-    neaderNav.addEventListener('click', (event: Event) => {
-      if ((event.target as HTMLElement).closest('.header__nav')) {
-        neaderNav?.classList.remove('header__nav--show');
-
-        burgerBtn?.classList.remove('burger-transform--top');
-        burgerCenterLine?.classList.remove('--hide');
-      }
-    });
-  };
-
+  // Burger Menu
   burger();
   clickOnOutside();
 
